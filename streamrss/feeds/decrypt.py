2602400ab532c4e60187abab37d2953ad5b1117b
0xPeterSatoshi <0xpetersatoshi@gmail.com>
2022-07-24 16:35:57 -0700

    feat: implement decrypt feed class

---
+++ streamrss/feeds/decrypt.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from streamrss.feeds.base import BaseFeed
+
+
+class Decrypt(BaseFeed):
+    """
+    Parses RSS feeds from decrypt.co.
+    """
+
+    feed_url: str = "https://decrypt.co/feed"
+    entries = None
+
+    def get_entries(self) -> list:
+        self.parse()
+        self.entries = self.feed.entries
+        print(f"number of entries: {len(self.entries)}")
+        return self.get_content()
+
+    def get_content(self) -> list:
+        new_entries = []
+        for entry in self.entries:
+            new_entries.append(self._get_entry_content(entry))
+
+        return new_entries
+
+    @staticmethod
+    def _get_entry_content(entry: dict) -> dict:
+        """
+        Returns content for an entry.
+
+        :param entries: A feedparser dict representing an entry
+        """
+        with requests.Session() as session:
+            print(f"making GET request for URL: {entry.link}")
+            response = session.get(entry.link)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, "html5lib")
+            content = soup.find_all("div", {"class": "z-2"})
+            entry["content"] = str(content)
+
+        return entry
+
