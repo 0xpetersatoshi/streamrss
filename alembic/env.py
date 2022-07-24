e5f7767d73c34f00986eb7b40bc1957fbb9dcf87
0xPeterSatoshi <0xpetersatoshi@gmail.com>
2022-07-24 16:32:18 -0700

    chore: update alembic scripts and migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,24 +1,36 @@
+import os
+import sys
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
+from streamrss.server.models import Base
 
 from alembic import context
+
+BASE_DIR= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+print(f"loading file:{os.path.join(BASE_DIR, '.env')}")
+sys.path.append(BASE_DIR)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+#  Making a connection
+config.set_main_option('sqlalchemy.url', os.environ['DATABASE_URL'])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
