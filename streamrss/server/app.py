a1c4932e33624d67b0811bf5b8ff60613d1517db
0xPeterSatoshi <0xpetersatoshi@gmail.com>
2022-07-26 15:03:18 -0700

    feat: add and implement stream endpoint

--- streamrss/server/app.py
+++ streamrss/server/app.py
@@ -1,8 +1,6 @@
-import asyncio
 import os
-from datetime import datetime
-from typing import List
 
+import nats
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
@@ -10,12 +8,13 @@ from fastapi_sqlalchemy import DBSessionMiddleware, db
 from sse_starlette.sse import EventSourceResponse
 from streamrss.server import crud, models, schemas
 
-
 BASE_DIR= os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-MESSAGE_STREAM_DELAY = 15  # second
+MESSAGE_STREAM_DELAY = 1  # second
 MESSAGE_STREAM_RETRY_TIMEOUT = 15000  # milisecond
+NATS_SERVER_URL = os.environ['NATS_SERVER_URL']
+FEED_SUBSCRIPTION_SUBJECT = os.environ['FEED_SUBSCRIPTION_SUBJECT']
 
 app = FastAPI()
 
@@ -34,11 +33,36 @@ async def rule():
     rule = db.session.query(models.Rules).all()
     return {"rules": rule}
 
-# TODO: use redis pubsub capabilities to publish new feeds to redis and implement
-# logic to check if there are new messages and read them from redis if so
+def get_messages(pubsub):
+    while True:
+        message = pubsub.get_message()
+        if message:
+            yield message
+
 @app.get('/stream')
 async def message_stream(request: Request):
-    pass
+    # Connect to nats server
+    nc = await nats.connect(NATS_SERVER_URL)
+
+    # Create JetStream context.
+    js = nc.jetstream()
+    sub = await js.subscribe(FEED_SUBSCRIPTION_SUBJECT, ordered_consumer=True)
+
+    async def event_generator():
+        while True:
+            # If client closed the connection
+            if await request.is_disconnected():
+                break
+
+            try:
+                async for msg in sub.messages:
+                    data = msg.data.decode()
+                    print(f"Received a message on '{msg.subject}': {data}")
+                    yield {"event": FEED_SUBSCRIPTION_SUBJECT, "data": data}
+            except Exception as e:
+                print(str(e))
+
+    return EventSourceResponse(event_generator())
 
 
 if __name__ == '__main__':
